ddf53f3dc8871c9bb3826d76ae80981301c799d0
Olawumi Olusegun <olawumi.olusegun@gmail.com>
2024-11-12 15:17:10 +0100

    Update: Added produce item images..

--- src/pages/produce.tsx
+++ src/pages/produce.tsx
@@ -45,7 +45,7 @@ const Produce = () => {
                     <p className="text-gray-800">
                         Maize (Zea mays) is the third most important cereal grain in the world, after wheat and rice, providing nutrients for humans and animals and serving as a basic raw material for production of starch, oil, protein, alcoholic beverages, food sweeteners, and fuel.
                     </p>
-                    <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary">
+                    <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary w-fit">
                         <span>Trade</span>
                         <svg className="group-hover:translate-x-1 duration-300" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M3.125 10H16.875" stroke="#3E933E" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
@@ -87,7 +87,7 @@ const Produce = () => {
                     <p className="text-gray-800">
                         Sesame seeds are an excellent source of manganese and calcium, both of which help your bones grow healthy and strong. Calcium also plays a role in nerve signal transmission, muscle movement, blood vessel function, and hormone release. Other vitamins and minerals found in sesame seeds include: Phosphorous.
                     </p>
-                    <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary">
+                    <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary w-fit">
                         <span>Trade</span>
                         <svg className="group-hover:translate-x-1 duration-300" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M3.125 10H16.875" stroke="#3E933E" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
@@ -129,7 +129,7 @@ const Produce = () => {
                     <p className="text-gray-800">
                         Sorghum is known to be rich in phenolic compounds, many of which act as antioxidants. It has also been shown to be good at reducing some forms of inflammation due to its antioxidant properties. Several of the phenolic compounds in sorghum have been linked to anti-cancer effects.
                     </p>
-                    <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary">
+                    <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary w-fit">
                         <span>Trade</span>
                         <svg className="group-hover:translate-x-1 duration-300" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M3.125 10H16.875" stroke="#3E933E" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
@@ -171,7 +171,7 @@ const Produce = () => {
                     <p className="text-gray-800">
                         Paddy rice isn't just a staple food; it's a nutritional marvel. Rich in carbohydrates, vitamins, and minerals, paddy rice fuels your body with sustained energy. Moreover, it supports digestive health and is gluten-free, making it an ideal choice for many diets.
                     </p>
-                    <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary">
+                    <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary w-fit">
                         <span>Trade</span>
                         <svg className="group-hover:translate-x-1 duration-300" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M3.125 10H16.875" stroke="#3E933E" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
@@ -213,7 +213,7 @@ const Produce = () => {
                     <p className="text-gray-800">
                         Soya Beans are a nutritional powerhouse, packed with protein, fiber, vitamins, and minerals. Incorporating Soya Beans into your diet can promote heart health, support bone strength, and aid in weight management. From tofu to soy milk, the versatility of Soya Beans offers endless culinary possibilities for a healthier lifestyle.
                     </p>
-                    <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary">
+                    <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary w-fit">
                         <span>Trade</span>
                         <svg className="group-hover:translate-x-1 duration-300" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M3.125 10H16.875" stroke="#3E933E" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
@@ -256,7 +256,7 @@ const Produce = () => {
                         Millet is rich in dietary fiber, both soluble and insoluble. The insoluble fiber in millet is known as a “prebiotic,” which means it supports good bacteria in your digestive system. This type of fiber is also important for adding bulk to stools, which helps keep you regular and reduces your risk of colon cancer.
                     </p>
                     <span className="text-gray-700 mt-auto">Coming soon...</span>
-                    {/* <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary">
+                    {/* <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary w-fit">
                         <span>Trade</span>
                         <svg className="group-hover:translate-x-1 duration-300" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M3.125 10H16.875" stroke="#3E933E" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
@@ -299,7 +299,7 @@ const Produce = () => {
                         Ginger helps speed up the digestion process and empty your stomach more quickly, improves Immunity, alleviates PMS symptoms, relieves nausea and upset stomach, reduces pain, healthier skin, weight loss aid, curbs cancer growth, and lowers blood sugar.
                     </p>
                     <span className="text-gray-700 mt-auto">Coming soon...</span>
-                    {/* <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary">
+                    {/* <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary w-fit">
                         <span>Trade</span>
                         <svg className="group-hover:translate-x-1 duration-300" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M3.125 10H16.875" stroke="#3E933E" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
@@ -343,7 +343,7 @@ const Produce = () => {
                         It has been confirmed to have several therapeutic actions, such as being hypoglycemic, antidiabetic, antioxidant, immunomodulatory, and antiulcer.
                     </p>
                     <span className="text-gray-700 mt-auto">Coming soon...</span>
-                    {/* <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary">
+                    {/* <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary w-fit">
                         <span>Trade</span>
                         <svg className="group-hover:translate-x-1 duration-300" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M3.125 10H16.875" stroke="#3E933E" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
@@ -387,7 +387,7 @@ const Produce = () => {
                         Cashew nut oil is rich in selenium, zinc, magnesium, iron and phosphorous.
                     </p>
                     <span className="text-gray-700 mt-auto">Coming soon...</span>
-                    {/* <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary">
+                    {/* <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary w-fit">
                         <span>Trade</span>
                         <svg className="group-hover:translate-x-1 duration-300" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M3.125 10H16.875" stroke="#3E933E" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
@@ -430,7 +430,7 @@ const Produce = () => {
                         Coffee contains antioxidants and other active substances that may reduce internal inflammation and protect against disease, healthy for your heart, may help curb certain cancers, may lessen your risk of developing type 2 diabetes, relieves mental and physical fatigue and to increase mental alertness.
                     </p>
                     <span className="text-gray-700 mt-auto">Coming soon...</span>
-                    {/* <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary">
+                    {/* <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary w-fit">
                         <span>Trade</span>
                         <svg className="group-hover:translate-x-1 duration-300" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M3.125 10H16.875" stroke="#3E933E" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
@@ -473,7 +473,7 @@ const Produce = () => {
                         Cocoa powder is rich in theobromine, which helps to reduce inflammation and can protect you from diseases such as heart disease, cancer, and diabetes. Since cocoa is rich in phytonutrients but low in fat and sugar, the calories you get from cocoa powder will be packed with healthy chemicals.
                     </p>
                     <span className="text-gray-700 mt-auto">Coming soon...</span>
-                    {/* <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary">
+                    {/* <Link to={"#"} className="flex items-center gap-1 group mt-auto text-primary w-fit">
                         <span>Trade</span>
                         <svg className="group-hover:translate-x-1 duration-300" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M3.125 10H16.875" stroke="#3E933E" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
