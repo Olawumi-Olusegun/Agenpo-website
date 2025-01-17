c2f11dba82cb1adec9f875c7e835964b34f7b037
Ibekason Alexander <alibekason@gmail.com>
2025-01-17 14:41:23 +0100

    make deploymennt script more dynamic

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -102,7 +102,7 @@ const Footer = () => {
             <div className="border-t" />
             <div className="py-8 max-w-[1280px] mx-auto px-5 lg:px-0">
                 <p className="text-gray-600 text-center lg:text-left">
-                    &copy; {new Date().getFullYear()} All Rights Reserved.
+                    &copy; {new Date().getFullYear()}   All Rights Reserved.
                 </p>
             </div>
         </footer>
